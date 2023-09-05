87b15eaf11c80b93a8ef1399e6437d35587d7c8b
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-09-05 23:44:11 +0500

    Update question5.c

--- Labs/lab03/question5.c
+++ Labs/lab03/question5.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+   int total_distance;
+   const int fuel_average = 2;
+   int fuel_consumption;
+   total_distance = 10 + 10 + 5 + 15;
+   fuel_consumption = total_distance * fuel_average;
+   printf("the total fuel consumed is: %d\n",fuel_consumption);
+   
+    return 0;
+}
