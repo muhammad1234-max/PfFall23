058c42883d1324e2e056a9c135268b2cbcbb1151
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-09-05 10:22:16 +0500

    Update question3.c

--- Labs/lab03/question3.c
+++ Labs/lab03/question3.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+#include <math.h>
+main() {
+    int a;
+    int b;
+    float hypotenuse;
+    printf("enter value of side A: ");
+    scanf("%d",&a);
+    printf("enter value of side B: ");
+    scanf("%d",&b);
+    hypotenuse = sqrt((a*a)+(b*b));
+    printf("the hypotenuse is: ");
+    printf("%f",hypotenuse);
+}
