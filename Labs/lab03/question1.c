d64530984de233e3343ec2ae151274432db96934
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-09-05 23:37:24 +0500

    Update question1.c

--- Labs/lab03/question1.c
+++ Labs/lab03/question1.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main() {
+    int a;
+    int b;
+    char oper;
+    int answer;
+
+    printf("Enter first num: ");
+    scanf("%d", &a);
+
+    printf("Enter second num: ");
+    scanf("%d", &b);
+
+   
+    printf("Enter desired operation: ");
+    scanf(" %c", &oper);  
+
+    if (oper == '+')
+        answer = a + b;
+    else if (oper == '-')
+        answer = a - b;
+    else if (oper == '*')
+        answer = a * b;
+    else if (oper == '/')
+        answer = a / b;
+    else
+        printf("Invalid operation\n");
+
+    if (oper == '+' || oper == '-' || oper == '*' || oper == '/')
+        printf("Result: %d\n", answer);
+
+    return 0;
+}
