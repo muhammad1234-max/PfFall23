ad4cccca3dc5a763d582da318efded60b33ec945
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-09-05 10:27:06 +0500

    Update question4.c

--- Labs/lab03/question4.c
+++ Labs/lab03/question4.c
@@ -1,1 +1,9 @@
-
+#include <stdio.h>
+main() {
+    int original_price=1500;
+    float discount=0.2;
+    int new_price;
+    new_price= original_price * discount;
+    printf("the final price is: ");
+    printf("%d",new_price);
+}
