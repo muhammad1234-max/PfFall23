e14597d3227f8e33e8af84a2e38c0e570178823f
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-09-12 10:51:13 +0500

    Update Question1.c

--- Labs/lab04/Question1.c
+++ Labs/lab04/Question1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    int n, x, y;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    x = 1;
+    while (x <= n) {
+        y = 1;
+        while (y <= x) {
+            printf("*");
+            y++;
+        }
+        printf("\n");
+        x++;
+    }
+
+}
