a49af1850ca4dddfc771431942603fa2cd8c5235
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-09-19 08:37:31 +0500

    Create ans1.c

---
+++ Labs/lab5/ans1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main () {
+	int num;
+	printf("enter num: ");
+	scanf("%d",&num);
+	if (num%3==0)
+	printf("this number is multiple of 3");
+	else 
+	printf("this number is not multiple of 3");
+}
