34ae4eb4af01393cb925cd8f25075b0b5ef3898e
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-10-03 10:07:49 +0500

    Create question3.c

---
+++ Labs/lab6/question3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, LCM = 1, GCD = 1, f = 2; 
+
+    printf("Enter num1: ");
+    scanf("%d", &num1);
+
+    printf("Enter num2: ");
+    scanf("%d", &num2);
+
+    // Calculate GCD
+    while (f <= num1 && f <= num2) {
+        if (num1 % f == 0 && num2 % f == 0) {
+            GCD = GCD * f;
+            num1 = num1 / f;
+            num2 = num2 / f;
+        } else {
+            f++;
+        }
+    }
+
+    // Calculate LCM
+    LCM = (num1 * num2 * GCD);
+
+    printf("GCD: %d\n", GCD);
+    printf("LCM: %d\n", LCM);
+
+    return 0;
+}
