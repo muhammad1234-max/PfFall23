76ae8bee558127b1d1afd095cbaac087266d1bbf
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-10-03 11:07:33 +0500

    Update question4.c

--- Labs/lab6/question4.c
+++ Labs/lab6/question4.c
@@ -1,60 +1,53 @@
-/**
- * Programmer: Muhammad Abbas
- * Desc: question4
- * Date: 10/3/23
-*/
-
 #include <stdio.h>
-
 int main() {
-    int a,b;
-    printf("enter a and b: ");
-    scanf("%d %d",&a,&b);
-    if (a<b) {
-    while (a==b) {
-        a++;
-    }
-    } else {
-        printf("wrong input\n");
-    }
-    if (a<=9) {
-        switch (a) {
-            case 1 :
-            printf("one");
-            break;
-            case 2:
-            printf("two");
-            break;
-            case 3:
-            printf("three");
-            break;
-            case 4:
-            printf("four");
-            break;
-            case 5:
-            printf("five");
-            break;
-            case 6:
-            printf("six");
-            break;
-            case 7:
-            printf("seven");
-            break;
-            case 8:
-            printf("eight");
-            break;
-            case 9:
-            printf("nine");
-            break;
 
-        }
-    } else {
-        if(a%2==0) {
-            printf("num is even");
-        } else {
-            printf("num is odd");
-        }
-    } 
+  int i=2, n=0,z=0;
+  int a,b;
+  printf("enter number 1");
+  scanf("%d",&a);
+   printf("enter number 2");
+  scanf("%d",&b);
+ while(a<=b){
+ 
+  while(a<=9)
+  {
+  switch(a)
+  {
+  case 1:
+  printf("one");
+  break;
+  case 2:
+  printf("two");
+  break;
+        case 3:
+  printf("three");
+  break;
+        case 4:
+  printf("four");
+  break;
+        case 5:
+  printf("five");
+  break;
+        case 6:
+  printf("six");
+  break;
+        case 7:
+  printf("seven");
+  break;
+        case 8:
+  printf("eight");
+  break;
+         case 9:
+  printf("nine");
+  break;
+ }
+a++;}
+ if(a%2==0)
+ {
+  printf("even");
+ }
+ else
+ printf("odd");
+  a++;
+ } }
 
-    return 0;
-}
