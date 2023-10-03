cba1c94b7bca65175624232cfd92a1cd4904b682
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-10-03 09:30:07 +0500

    Create question2.c

---
+++ Labs/lab6/question2.c
@@ -0,0 +1,41 @@
+/**
+ * Programmer: Muhammad Abbas
+ * Desc: question2
+ * Date: 10/3/23
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num1 = 1, num2 = 1, nextTerm;
+    int sum = 0;
+
+
+    printf("Fibonacci series up to 10000:\n");
+    printf("%d %d ", num1, num2);
+
+    for (nextTerm = num1 + num2; nextTerm <= 10000;) {
+        printf("%d ", nextTerm);
+        num1 = num2;
+        num2 = nextTerm;
+        nextTerm = num1 + num2;
+    }
+
+
+    num1 = 1;
+    num2 = 1;
+    nextTerm = num1 + num2;
+
+    for (; nextTerm <= 10000;) {
+        if (nextTerm % 3 == 0 || nextTerm % 5 == 0 || nextTerm % 7 == 0) {
+            sum = sum+ nextTerm;
+        }
+        num1 = num2;
+        num2 = nextTerm;
+        nextTerm = num1 + num2;
+    }
+
+    printf("\nSum of Fibonacci numbers divisible by 3, 5, or 7: %d\n", sum);
+
+    return 0;
+}
