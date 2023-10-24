b239a59b2bb14177644485e6460440efa61021d2
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-10-24 09:18:44 +0500

    Update question3.c

--- Labs/lab08/question3.c
+++ Labs/lab08/question3.c
@@ -8,31 +8,30 @@ occurrences of the character in the input text.
 Date: 24/10/23
 */
 
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+	//an array to store string
+    char text[100];
+    char searchChar;
+    int count = 0,i;
+    
+    //enter the string
+    printf("Enter a string: ");
+    fgets(text, sizeof(text), stdin);
+    
+    printf("Enter a character to search for: ");
+    scanf(" %c", &searchChar); // Note the space before %c to consume any leading whitespace.
+    
+    //looping the whole string to find the character
+    for (i = 0;/*increment to access next location*/ text[i] != '\0'; i++) {
+        if (text[i] == searchChar) {
+            count++;
+        }
+    }
 
-main() {
-	int string[100];
-	char c;
-	int count=0;
-	
-	//entering a string in a array of characters
-	printf("enter the string you want to check:\n");
-	fgets(string,sizeof(string),stdin);
-	
-	printf("enter the character you want to find:");
-	scanf("%c",&c);
-	
-	//definfing a pointer variable
-	char *foundchar = strchr(string,c);
-	//if char not found the function return a null character
-	while(foundchar!=NULL) {
-		count = count+1;
-		foundchar = strchr(/*this will make the loop check the  next address location*/foundchar + 1,c);
-	}
-	
-	printf("Total occurrences of '%c' in the text: %d\n", c, count);
-	
-	
+    printf("Total occurrences of '%c' in the text: %d\n", searchChar, count);
+
+    return 0;
 }
