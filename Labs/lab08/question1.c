562d02a3b3ab13caee64fe715b6a089e23673d5f
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2023-10-24 08:27:30 +0500

    Create question1.c

---
+++ Labs/lab08/question1.c
@@ -0,0 +1,26 @@
+uses the function to swap them. It should perform the swap, and display the
+updated values.
+Date: 24/10/23
+*/
+
+#include<stdio.h>
+
+//a function to swap two integers
+void swapInteger(int num1, int num2) {
+	int temp;
+	temp=num1;
+	num1=num2;
+	num2=temp;
+	printf("%d %d",num1,num2);
+}
+
+main() {
+	int one,two;
+	printf("enter two numbers:\n");
+	scanf("%d %d",&one,&two);
+	
+	printf("the two numbers are %d and %d\n",one,two);
+	printf("after applying swap function:\n");
+	//calling the function
+	swapInteger(one,two);
+}
